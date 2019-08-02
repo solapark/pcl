fad0588ac149c19ef5b02083a976370050f496b8
sola <opop0628@gmail.com>
2019-08-02 17:59:15 +0900

    plot_pcl

---
+++ plot_pcl.py
@@ -0,0 +1,12 @@
+import pcl
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+p = pcl.load("PointCloud_namuga/00.ply")
+p_np = np.asarray(p)
+x, y, z = p_np[:,0], p_np[:, 1], p_np[:, 2]
+fig = plt.figure(figsize=(10, 5))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y, z, c = z, s= 20, alpha=0.5, cmap=plt.cm.Greens)
+plt.show()
