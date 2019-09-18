31a752ef71f4ed8624ecd24220d32ae0c476f73f
sola <opop0628@gmail.com>
2019-09-18 14:28:06 +0900

    can deal with color

--- make_error.py
+++ make_error.py
@@ -3,20 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def get_error_path(org_path, is_add, is_del, add_prob, del_prob) :
-    name, exp = org_path.split('.')
+pcl_path ="/home/sap/namuga/PointCloud_Web/longdress.ply"
+new_dir ="/home/sap/namuga/PointCloud_Web/longdress_error_refine/"
+
+is_add =1 
+rand_lowest = -1
+rand_highest = 1
+add_prob = 0.1
+
+is_del = 0
+del_prob = .5
+
+def get_error_path(org_path, is_add, is_del, add_prob, del_prob, new_dir) :
+    name_exp = org_path.split('/')[-1]
+    name, exp = name_exp.split('.')
     if(is_add) :
         add_prob_percent = int(add_prob * 100)
-        new_path = name+"_add_"+str(add_prob_percent)+'.'+exp
+        new_name = name+"_add_"+str(add_prob_percent)+'.'+exp
     if(is_del) :
         del_prob_percent = int(del_prob * 100)
-        new_path = name+"_del_"+str(del_prob_percent)+'.'+exp
+        new_name = name+"_del_"+str(del_prob_percent)+'.'+exp
+    new_path = new_dir + new_name
 
     return new_path
 
 def add_error(pcl_org, rand_lowest, rand_highest, add_prob):
     pcl_size = pcl_org.shape
     rand_int = np.random.randint(rand_lowest, rand_highest+1, size=pcl_size) #(p_np.shape)
+    rand_int[:, -1] = 0 #color information
     p = [1-add_prob, add_prob]
     is_add = np.random.choice([0,1], (pcl_size[0], 1), p=p) #(p_np.shape)
     rand = rand_int * is_add
@@ -34,20 +48,11 @@ def del_error(pcl_org, del_prob):
     return new_pcl
 
 if __name__=="__main__":
-    pcl_path ="PointCloud_Web/longdress.ply"
-
-    is_add = 1
-    rand_lowest = -1
-    rand_highest = 1
-    add_prob = 0.5
-
-    is_del = 0
-    del_prob = .5
-
-    error_pcl_path =get_error_path(pcl_path, is_add, is_del, add_prob, del_prob) 
+    error_pcl_path =get_error_path(pcl_path, is_add, is_del, add_prob, del_prob, new_dir) 
      
-    p = pcl.load(pcl_path)
-    p_np = np.asarray(p)
+    p = pcl.load_XYZRGBA(pcl_path)
+    #p_np = np.asarray(p)
+    p_np = p.to_array()
     if is_add :
         new_pcl = add_error(p_np, rand_lowest, rand_highest, add_prob) 
     elif is_del :
@@ -55,6 +60,6 @@ if __name__=="__main__":
     print('pcl', p_np.shape)
     print('new_pcl', new_pcl.shape)
 
-    cloud = pcl.PointCloud()
+    cloud = pcl.PointCloud_PointXYZRGBA()
     cloud.from_array(new_pcl)
     pcl.save(cloud, error_pcl_path, format="ply")
